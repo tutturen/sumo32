a83b7d5c5ac14dbc751a52aca12913dc2e798eea
Thor Even Tutturen <t.tutturen@gmail.com>
2016-11-07 12:45:55 +0100

    change to sensorprinter class

--- src/test.py
+++ src/test.py
@@ -4,40 +4,33 @@ import library.reflectance_sensors as reflectance
 import library.irproximity_sensor as ir
 import time
 
-# ultrasonic
-def print_ultrasonic():
-    sensor = ultrasonic.Ultrasonic()
-    sensor.update()
-    value = sensor.get_value()
-    print(value)
+class SensorPrinter():
+    def __init__(self):
+        self.ultrasonic_sensor = ultrasonic.Ultrasonic()
+        self.ir = ir.IRProximitySensor()
+        self.reflectance_sensor = reflectance.ReflectanceSensors(True)
 
-# ir-proximity
-def print_ir_proximity():
-    sensor = ir.IRProximitySensor()
-    sensor.update()
-    value = sensor.get_value()
-    print(value)
+    def print_ultrasonic(self):
+        self.ultrasonic_sensor.update()
+        value = self.ultrasonic_sensor.get_value()
+        print(value)
 
-# reflectance
-calibrated = False
-def print_reflectance():
-    global calibrated
-    sensor = reflectance.ReflectanceSensors()
-    if not calibrated:
-        for i in range(5):
-            sensor.calibrate()
-            time.sleep(1)
-        calibrated = True
-    sensor.update()
-    value = sensor.get_value()
-    print(value)
+    def print_ir_proximity(self):
+        self.ir.update()
+        value = self.ir.get_value()
+        print(value)
+
+    def print_reflectance(self):
+        self.reflectance_sensor.update()
+        value = self.reflectance_sensor.get_value()
+        print(value)
 
 if __name__ == '__main__':
-    print('Testing ultrasonic...')
-    #robodemo.dancer()
+    print('Testing sensors...')
+    printer = SensorPrinter()
     while True:
-        print_ultrasonic()
-        print_ir_proximity()
-        print_reflectance()
+        printer.print_ultrasonic()
+        printer.print_ir_proximity()
+        printer.print_reflectance()
         time.sleep(1)
 
