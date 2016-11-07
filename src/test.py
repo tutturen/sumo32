32957f0add60c7858ea2609792cc94f751ea7404
Thor Even Tutturen <t.tutturen@gmail.com>
2016-11-07 12:24:33 +0100

    add ultrasonic test

--- src/test.py
+++ src/test.py
@@ -1,9 +1,23 @@
 import library.robodemo as robodemo
+import library.ultrasonic as ultrasonic
+import library.reflectance_sensors as reflectance
+import library.irproximity_sensor as ir
+import time
+
+# ultrasonic
+def print_ultrasonic():
+    sensor = ultrasonic.Ultrasonic()
+    sensor.update()
+    value = sensor.get_value()
+    print(value)
+# ir-proximity
+
+# reflectance
 
 if __name__ == '__main__':
-    print('Doing the tourist demo. Just press the button...')
+    print('Testing ultrasonic...')
     #robodemo.dancer()
-    robodemo.tourist()
+    while True:
+        print_ultrasonic()
+        time.sleep(1)
 
-def ha():
-    return
