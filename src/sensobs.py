988ee751574fa53790fb9ad3d006b518846f238e
Erlend Åmdal <erlend@aamdal.com>
2016-11-09 13:19:17 +0100

    Remove update from sensobs

--- src/sensobs.py
+++ src/sensobs.py
@@ -6,10 +6,6 @@ class Sensob(ABC):
         pass
 
     @abstractmethod
-    def update(self):
-        pass
-
-    @abstractmethod
     def get_value(self):
         pass
 
@@ -17,9 +13,6 @@ class Sensob(ABC):
 class ObjectSideProximity(Sensob):
     def __init__(self, irporoximity_sensor):
         self.ir_proximity_sensors = irporoximity_sensor
-
-    def update(self):
-        return self.ir_proximity_sensors.update()
 
     def get_value(self):
         return self.ir_proximity_sensors.get_value()
@@ -31,9 +24,6 @@ class PickedUp(Sensob):
         # sets the treshold sum for the sensors for which the robot will take a picture
         self.reflectance_treshold = reflectance_treshold
 
-    def update(self):
-        self.reflectance_sensors.update()
-
     def get_value(self):
         values = self.reflectance_sensors.get_value()
         return sum(values) < self.reflectance_treshold
@@ -44,9 +34,6 @@ class UltrasonicTracking(Sensob):
         self.ultrasonic_sensor = ultrasonic_sensors
         self.max_tracking_distance_cm = max_tracking_distance_cm
         self.min_tracking_distance_cm = min_tracking_distance_cm
-
-    def update(self):
-        return self.ultrasonic_sensor.update()
 
     def get_value(self):
         value = self.ultrasonic_sensor.get_value
