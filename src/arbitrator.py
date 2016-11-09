d381dc862299c8c181a5da385ed731eba6cfc514
Erlend Åmdal <erlend@aamdal.com>
2016-11-09 11:03:31 +0100

    Add arbitrator

---
+++ src/arbitrator.py
@@ -0,0 +1,14 @@
+import random
+
+
+class Arbitrator:
+    def choose_behaviour(self, behaviours):
+        behaviour_weights = [behaviour.get_priority_weight() for behaviour in behaviours]
+        total_weight = sum(behaviour_weights)
+
+        rand_choice = random.random() * total_weight
+        accumulated_weight = 0
+        for i, behaviour_weight in enumerate(behaviour_weights):
+            accumulated_weight += behaviour_weight
+            if rand_choice <= accumulated_weight:
+                return behaviours[i]
