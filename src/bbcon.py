9a97dc17650eba8d2a127d120f5194fa41441e9b
Thor Even Tutturen <t.tutturen@gmail.com>
2016-11-09 13:29:09 +0100

    add sensob_controller

--- src/bbcon.py
+++ src/bbcon.py
@@ -2,10 +2,32 @@ import time
 from arbitrator import Arbitrator
 import motobs
 
+from library.irproximity_sensor import IRProximitySensor
+from library.reflectance_sensors import ReflectanceSensors
+from library.ultrasonic import Ultrasonic
+import sensobs
+
+class SensObController():
+
+    def __init__(self):
+        self.sensor = {
+            'proximity': IRProximitySensor(),
+            'reflectance': ReflectanceSensors(),
+            'ultrasonic': Ultrasonic()
+        }
+        self.object_side_proximity = sensobs.ObjectSideProximity(self.sensor['proximity'])
+        self.picked_up = sensobs.PickedUp(self.sensor['reflectance'], 1)
+        self.ultrasonic_tracking = sensobs.UltrasonicTracking(self.sensor['ultrasonic'], 100, 10)
+
+    def update(self):
+        # Update all sensors one time
+        for key in self.sensor:
+            self.sensor[key].update()
+
 class BBCon:
 
-    def __init__(self, behaviors):
-        self.sensobs = []
+    def __init__(self, sensob_controller, behaviors):
+        self.sensob_controller = sensob_controller
         self.behaviors = behaviors
         self.arbitrator = Arbitrator()
         self.motobs = [motobs.MotOb()]
@@ -20,8 +42,11 @@ class BBCon:
     def run_one_timestep(self):
         # Update all sensobs
         print('Updating sensobs')
-        for sensob in self.sensobs:
-            sensob.update()
+        self.sensob_controller.update()
+
+        # Update all behaviors
+        # for behavior in self.active_behaviors:
+        #     behavior.update()
 
         # Let arbitrator choose action
         chosen_behavior = self.arbitrator.choose_behavior(self.behaviors)
